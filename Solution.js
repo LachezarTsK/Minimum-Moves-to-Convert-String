a0b0caaf777edf3c12938f6d90e66cd842a652d2
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-09-25 20:19:15 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,20 @@
+
+/**
+ * @param {string} input
+ * @return {number}
+ */
+var minimumMoves = function (input) {
+    const NUMBER_OF_CONSECUTIVE_CHARS_TO_BE_REMOVED_AT_ONCE = 3;
+    const TARGET_CHAR = 'O';
+    const CHAR_TO_CONVERT_TO_TARGET = 'X';
+
+    let minMovesToConvertAllChars = 0;
+
+    for (let i = 0; i < input.length; ++i) {
+        if (input.charAt(i) === CHAR_TO_CONVERT_TO_TARGET) {
+            i += NUMBER_OF_CONSECUTIVE_CHARS_TO_BE_REMOVED_AT_ONCE - 1;
+            ++minMovesToConvertAllChars;
+        }
+    }
+    return minMovesToConvertAllChars;
+};
